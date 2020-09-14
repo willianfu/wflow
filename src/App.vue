60b0ed02f44b9ea4a1d5251543b9604d139bec71
willianfu <2423542142@qq.com>
2020-09-14 23:15:16 +0800

    更新静态文件

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,40 @@
 <template>
-	<div id="app">
-		<transition name="router-fade" mode="out-in">
-			<router-view v-if="!$route.meta.keepAlive"/>
-		</transition>
-	</div>
+  <div id="app">
+    <transition name="router-fade" mode="out-in">
+      <router-view v-if="!$route.meta.keepAlive"/>
+    </transition>
+  </div>
 </template>
 
 <script>
-    //import layout from './components/layout'
-    export default {
-        components: {},
-        data() {
-					return{}
-        },
-        methods: {}
-    }
+  //import layout from './components/layout'
+  export default {
+    components: {},
+    data() {
+      return {}
+    },
+    mounted() {
+			this.$router.push("/layout/formDesign")
+    },
+    methods: {}
+  }
 </script>
 <style lang="less">
-	@import "@/assets/global";
-	
-	body {
-		margin: 0;
-		min-width: 1000px;
-		background-color: #f5f6f6;
-	}
-	ul{
-		padding: 0;
-		margin: 0;
-		li{
-			list-style-type: none;
-		}
-	}
-	
+  @import "@/assets/global";
+
+  body {
+    margin: 0;
+    min-width: 1000px;
+    background-color: #f5f6f6;
+  }
+
+  ul {
+    padding: 0;
+    margin: 0;
+
+    li {
+      list-style-type: none;
+    }
+  }
+
 </style>
